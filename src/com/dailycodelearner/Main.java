481dfd444c2374469fdc2fe7a93e187ffbc3ed39
vishnu <bvishnu123vardhan@gmail.com>
2023-06-30 00:30:21 +0530

    functionalinterface basics

--- src/com/dailycodelearner/Main.java
+++ src/com/dailycodelearner/Main.java
@@ -1,12 +1,25 @@
 package com.dailycodelearner;
 
-import com.dailycodelearner.lambda.LambdaExample;
+
+import java.time.LocalDateTime;
+import java.util.function.Consumer;
+import java.util.function.Function;
+import java.util.function.Supplier;
 
 public class Main {
     public static void main(String a[]){
         /**
-         * basic implementation of lambda expression
+         * functionInterfaces basic implementation
+         * function<T,R> takes input and return output,contains apply method
+         * conusmerinterface<t> takes input and uses it , contains accept method
+         * supplier<R> returns output , contains get method
          */
-       LambdaExample.shapeImplementation();
+
+        Function<String,Integer> function=(s)->s.length();
+        System.out.println(function.apply("suresh"));
+        Consumer<String> consumer=(s)-> System.out.println(s);
+        consumer.accept("vishnu");
+        Supplier<LocalDateTime> supplier=()->LocalDateTime.now();
+        System.out.println(supplier.get());
     }
 }
